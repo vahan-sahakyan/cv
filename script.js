e5474c401fcdf8b67d565b2899a51d744662bf84
Vahan Sahakyan <vahan.sahakyan1996@gmail.com>
2023-03-08 16:36:22 +0400

    updating the content

--- script.js
+++ script.js
@@ -9,17 +9,15 @@ const copyToClipboard = element => {
   window.getSelection().removeAllRanges(); // to deselect
 };
 const links = {
-  github: 'https://github.com/vahan-sahakyan',
-  linkedin: 'https://www.linkedin.com/in/vahan-sahakyan/',
+  '.ln-svg': 'https://github.com/vahan-sahakyan',
+  '.gh-svg': 'https://www.linkedin.com/in/vahan-sahakyan/',
 };
-const addGoTo = (element, url) => {
+Object.entries(links).forEach(([element, url]) => {
   document.querySelector(element).addEventListener('click', e => {
     e.preventDefault();
     window.open(url);
   });
-};
-addGoTo('.ln-svg', links.linkedin);
-addGoTo('.gh-svg', links.github);
+});
 
 let clipboardTimer;
 const message = document.querySelector('.clipboard-message');
@@ -28,9 +26,7 @@ const message = document.querySelector('.clipboard-message');
     //
     copyToClipboard(el);
 
-    message.textContent = `${
-      el[1].toUpperCase() + el.slice(2)
-    } Copied Successfully`;
+    message.textContent = `${el[1].toUpperCase() + el.slice(2)} Copied Successfully`;
     message.classList.remove('hidden');
 
     clearTimeout(clipboardTimer);
@@ -48,6 +44,6 @@ const setTheme = () => {
 
 setTheme();
 
-window
-  .matchMedia('(prefers-color-scheme: light)')
-  .addEventListener('change', setTheme);
+window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', setTheme);
+
+///////////////////////
