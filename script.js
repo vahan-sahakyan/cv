2962ab8c0bb3f15757ae6ea2b5977be3947fa78a
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2023-09-08 11:36:36 +0400

    making tweaks, adding console actions

--- script.js
+++ script.js
@@ -1,63 +1,135 @@
 'use strict';
 
-const LINKS = {
+init();
+
+///////////////////////
+// SIDEBAR LINKS
+
+/** @type {[element: string, url: string][]}  */
+const SOCIAL_LINKS = Object.entries({
   '.gh-svg': 'https://github.com/vahan-sahakyan',
   '.ln-svg': 'https://www.linkedin.com/in/vahan-sahakyan/',
-};
-Object.entries(LINKS).forEach(([element, url]) => {
-  document.querySelector(element).addEventListener('click', event => {
-    event.preventDefault();
-    window.open(url);
-  });
 });
 
+for (const linkTupleItem of SOCIAL_LINKS) attachOnClickUrlOpener(linkTupleItem);
+
+///////////////////////
+// CLIPBOARD
+
 let clipboardTimer;
-const message = document.querySelector('.clipboard-message');
+const msg = document.querySelector('.clipboard-message');
 ['.email', '.phone'].forEach(el => {
-  document.querySelector(el).addEventListener('click', function () {
-    //
-
-    navigator.clipboard.writeText(this.textContent.trim());
-
-    message.textContent = `${
-      el[1].toUpperCase() + el.slice(2)
-    } Copied Successfully`;
-    message.classList.remove('hidden');
-
+  document.querySelector(el).addEventListener('click', () => {
+    navigator.clipboard.writeText(this.textContent);
+    msg.textContent = `${classToWord(el)} Copied Successfully`;
+    msg.classList.remove('hidden');
     clearTimeout(clipboardTimer);
-    clipboardTimer = setTimeout(function () {
-      message.classList.add('hidden');
-    }, 1600);
+    clipboardTimer = setTimeout(() => msg.classList.add('hidden'), 1600);
   });
 });
 
 ///////////////////////
 // DARK MODE
-const setTheme = () => {
-  const isLight = window.matchMedia('(prefers-color-scheme: light)').matches;
-  document.getElementById('dark-css').disabled = isLight;
-};
 
-setTheme();
-
-window
-  .matchMedia('(prefers-color-scheme: light)')
-  .addEventListener('change', setTheme);
+onThemeChange(updateTheme);
 
 ///////////////////////
 // PDF MODE
 
-let pdfMode = false;
-function togglePdfMode() {
-  document.getElementById('pdf-mode-css').disabled = pdfMode;
-  pdfMode = !pdfMode;
-}
-let degree = 0;
-function rotate(times = 1) {
-  document.querySelector('.a4').style.transform = `rotate(${(degree +=
-    90 * times)}deg)`;
-}
-///////////////////////
+const togglePdfMode = (function () {
+  let pdfMode = false;
+  return function (enable = undefined) {
+    document.getElementById('pdf-mode-css').disabled =
+      enable !== undefined ? !enable : pdfMode;
+    pdfMode = enable ?? !pdfMode;
+  };
+})();
 
-// togglePdfMode();
-// rotate(3);
+const rotate = (function () {
+  let degree = 0;
+  return function (times = 1) {
+    document.querySelector('.a4').style.transform = `rotate(${(degree +=
+      90 * times)}deg)`;
+  };
+})();
+
+///////////////////////
+// HELPERS
+
+/** Initialize the page settings */
+function init() {
+  updateTheme();
+}
+/** Updates the theme based on prefered color-scheme */
+function updateTheme() {
+  const { matches: isLight } = themeMatchMedia();
+  document.getElementById('dark-css').disabled = isLight;
+}
+/** @returns {MediaQueryList} */
+function themeMatchMedia() {
+  return window.matchMedia('(prefers-color-scheme: light)');
+}
+/** @param {function} handler */
+function onThemeChange(handler) {
+  themeMatchMedia().addEventListener('change', handler);
+}
+/** @param {[string, string]} linkEntryTuple */
+function attachOnClickUrlOpener([element, url]) {
+  document
+    .querySelector(element)
+    .addEventListener('click', _e => window.open(url));
+}
+/** @param {string} str @returns {string} */
+function classToWord(str) {
+  return str.replace(
+    /\.(\w)(\w+)/g,
+    /** @param {...string} groups @returns {string} */
+    (_, ...groups) => groups[0].toUpperCase() + groups[1]
+  );
+}
+/** Switch between display modes: [true: screenshot, false: normal] */
+const screenshotMode = (function () {
+  let enabled = false;
+  return function screenshotMode(enable) {
+    if ([undefined, null].includes(enable))
+      return console.error(new UsageError(enable, 'boolean'));
+    if (enable && !enabled) {
+      enabled = !enabled;
+      togglePdfMode(true);
+      rotate(3);
+    } else if (!enable && enabled) {
+      enabled = !enabled;
+      togglePdfMode(false);
+      rotate(1);
+    } else if (!enable) {
+      togglePdfMode(false);
+    } else if (enable) {
+      togglePdfMode(true);
+    }
+    return 0;
+  };
+})();
+
+///////////////////////
+// CUSTOM EXCEPTIONS
+
+class UsageError extends Error {
+  constructor(passed, needed) {
+    super();
+    this.name = 'UsageException';
+    console.log(passed);
+    this.message = `
+      \nYou've passed "${passed !== null ? passed : null}" value.
+      \nTry to pass a ${needed} value instead
+    `;
+  }
+}
+
+///////////////////////
+// AVAILABLE ACTIONS
+
+window.actions = {
+  screenshotMode,
+  togglePdfMode,
+  rotate,
+};
